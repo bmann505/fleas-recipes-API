fc555cb62b55158dabd7b483ad89434e665b51de
Brian Mann <bmann505@gamil.com>
2017-08-18 10:49:59 -0600

    hashed passwords login/signup

--- db/users.js
+++ db/users.js
@@ -1,8 +1,9 @@
 const knex = require('./connection')
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
-function validUser(user) {
+function validNewUser(user) {
   let validName = typeof user.name === 'string' && user.name.trim() !== '';
   let validEmail = typeof user.email === 'string' && user.email.match(/([@])/g) != null;
   let validPassword = typeof user.password === 'string' && user.email.trim() !== '' && user.password.length > 4;
@@ -10,15 +11,18 @@ function validUser(user) {
 }
 
 router.post('/signup', (req, res) => {
-  console.log(req.body.password.length);
-  if (validUser(req.body)) {
+  if (validNewUser(req.body)) {
     knex('user')
       .where('email', req.body.email)
       .then(user => {
         if (user.length === 0) {
+          const saltRounds = 8;
+          const hash = bcrypt.hashSync(req.body.password, saltRounds);
+          req.body.password = hash;
           knex('user')
             .insert(req.body).returning('*')
             .then(user => {
+              delete user[0].password;
               res.json(user);
             })
         } else {
@@ -34,5 +38,22 @@ router.post('/signup', (req, res) => {
   }
 })
 
+router.post('/login', (req, res) => {
+  knex('user')
+    .where('email', req.body.email)
+    .then(user => {
+      if (user.length === 0) {
+        res.json({
+          error: 'email not found'
+        });
+      } else {
+        let match = bcrypt.compareSync(req.body.password, user[0].password);
+        if (match) {
+          delete user[0].password;
+          res.json(user);
+        }
+      }
+    })
+})
 
-module.exports = router;
+module.exports = router;;
